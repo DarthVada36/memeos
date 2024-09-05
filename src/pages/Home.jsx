516916d16b326ab788a460b477b0965e9d160b8b
Rada749 <radames5897@gmail.com>
2024-09-05 14:37:33 +0200

    Pull

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-<<<<<<< HEAD
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Mi vida es un meme</h1>
       <Link to='/gallery'>
         <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">Ir a la Exposición</button>
       </Link>
-=======
-    <div>
-      <h1 className='text-3xl'>Mi vida es un meme</h1>
->>>>>>> 36bd8bb4b4a6a390df0afb56b9471d6bc179491b
     </div>
   )
 }
