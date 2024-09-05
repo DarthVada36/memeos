e2d579988d77457851fff126a2e4ba7888ca5abd
DarthVada36 <velazquez.vada@outlook.com>
2024-09-06 01:36:29 +0200

    Config: Tailwind 1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+    colors: {
+      'darkGray': '#272525',
+      'midGray': '#4D4C47',
+      'bronze': '#9C7E41',
+      'lightGray': '#D3C9BE',
+      'white': '#EDE7E1',
+      },
+      fontFamily: {
+        sans: ['Raleway', 'sans-serif'],
+        serif: ['Libre Bodoni', 'serif'],
+      },
   plugins: [],
+    },
+  },
 }
-
