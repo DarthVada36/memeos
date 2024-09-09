7ea59990aa7a724f5b3d4e3a301aac9861ec43ed
Diallo2024 <urymamadu@gmail.com>
2024-09-09 10:59:49 -0700

    crud functions

--- src/services/services.js
+++ src/services/services.js
@@ -1,1 +1,34 @@
-//CRUD
+// src/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000', // URL base del json-server
+});
+
+
+// Funciones CRUD
+export const getMemes = async () => {
+  const response = await api.get('/memes');
+  return response.data;
+};
+
+export const createMeme = async (meme) => {
+  const response = await api.post('/memes', meme);
+  return response.data;
+};
+
+export const updateMeme = async (id, meme) => {
+  const response = await api.put(`/memes/${id}`, meme);
+  return response.data;
+};
+
+export const deleteMeme = async (id) => {
+  await api.delete(`/memes/${id}`);
+};
+
+
+//Importamos todo aqui para completar el CRUD.
+
+
+
+
