7e1a715480b1d48a73dfc709d1add70d3361031c
DarthVada36 <velazquez.vada@outlook.com>
2024-09-10 12:33:41 +0200

    add:navbar- logo

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = () => {
   return (
     <>
     <nav className="w-[1440px] h-[100px] bg-primary/25 flex justify-center content-between ">
-      <div>logo</div>
+      <div className='left-0'><img src="./src/img/dogelogo2.png" className="w-[68px] h-[68px]" alt="" /></div>
       <h3 className="text-bronze font-bodoni">Añadir meme</h3>
     </nav>
     <Outlet/>
