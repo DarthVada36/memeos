62168c12b010cf4d2ac823b2c34bfe3f4ef5ada2
DarthVada36 <velazquez.vada@outlook.com>
2024-09-10 10:03:05 +0200

    Tailwind style added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <h1 className='text-3xl'>Mi vida es un meme</h1>
+      <h1 className='text-3xl text-bronze font-bodoni'>Mi vida es un meme</h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
