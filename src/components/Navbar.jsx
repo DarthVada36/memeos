bc0938843337a6e359756410ed64df292ae2f088
Smith Grisales <developers.smith@gmail.com>
2024-09-12 11:19:49 +0200

    pull

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
-       <nav class="bg-primary p-4 fixed w-full top-0 z-10">
+      <nav class="bg-primary p-4 fixed w-full top-0 z-10">
         <div class="max-w-7xl mx-auto flex justify-between items-center">
-            <a href="#" class="text-bronze text-2xl font-bold">Meme</a>
-            <ul class="flex space-x-6">
-                <li><a href="#home" class="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded">Home</a></li>
-                <li><a href="#add" class="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded">Añadir</a></li>
-        
-        <a href="/" class="text-gray transition-colors duration-300 transform dark:text-gray-200  border-blue-500 mx-1.5 sm:mx-6"><img class="h-6 " src="../public/img-perro.svg" alt="Imagen Meme" /></a>
+          <a href="#" class="text-bronze text-2xl font-bold">
+            Meme
+          </a>
+          <ul class="flex space-x-6">
+            <li>
+              <a
+                href="#home"
+                class="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#add"
+                class="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
+              >
+                Añadir
+              </a>
+            </li>
 
-        <a href="/" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">
-        </a>
+            <a
+              href="/"
+              class="text-gray transition-colors duration-300 transform dark:text-gray-200  border-blue-500 mx-1.5 sm:mx-6"
+            >
+              <img
+                class="h-6 "
+                src="../public/img-perro.svg"
+                alt="Imagen Meme"
+              />
+            </a>
 
-        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">
-      
-        </a>
-            </ul>
+            <a
+              href="/"
+              class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+            ></a>
+
+            <a
+              href="#"
+              class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+            ></a>
+          </ul>
         </div>
-    </nav>
-    
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
-
-
-
-
+export default Navbar;
