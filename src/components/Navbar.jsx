f2e1af7134896d7adc0821eb79e8212c79e5ecd7
Smith Grisales <developers.smith@gmail.com>
2024-09-13 22:43:05 +0200

    resolve classname navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header classNameName="font-bodoni">
-      <nav classNameName="bg-primary p-4 fixed w-full top-0 z-10">
-        <div classNameName="max-w-7xl mx-auto flex justify-between items-center">
-          <a href="#" classNameName="text-bronze text-2xl font-bold">
+    <header className="font-bodoni">
+      <nav className="bg-primary p-4 fixed w-full top-0 z-10">
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+          <a href="#" className="text-bronze text-2xl font-bold">
             Meme
           </a>
-          <ul classNameName="flex space-x-6">
+          <ul className="flex space-x-6">
             <Link to="/gallery">
               <li>
                 <a
                   href="#home"
-                  classNameName="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
+                  className="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
                 >
                   Galería
                 </a>
@@ -24,7 +24,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#add"
-                  classNameName="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
+                  className="text-gray hover:bg-secondary hover:text-milk px-4 py-2 rounded"
                 >
                   Añadir
                 </a>
@@ -33,10 +33,10 @@ const Navbar = () => {
 
             <a
               href="/"
-              classNameName="text-gray transition-colors duration-300 transform dark:text-gray-200  border-blue-500 mx-1.5 sm:mx-6"
+              className="text-gray transition-colors duration-300 transform dark:text-gray-200  border-blue-500 mx-1.5 sm:mx-6"
             >
               <img
-                classNameName="h-6 "
+                className="h-6 "
                 src="../public/img-perro.svg"
                 alt="Imagen Meme"
               />
