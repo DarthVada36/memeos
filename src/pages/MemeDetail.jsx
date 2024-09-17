8186bae19743b70a75ad9bf1f248454bc52f2711
Rada749 <radames5897@gmail.com>
2024-09-17 14:44:59 +0200

    del border to img

--- src/pages/MemeDetail.jsx
+++ src/pages/MemeDetail.jsx
@@ -39,60 +39,60 @@ const MemeDetail = () => {
 
   return (
     <div className="bg-primary w-full flex-col lg:h-screen flex items-center mt-32">
-      <div className="flex-col font-raleway text-primary lg:bg-milk bg-gray lg:p-8 w-full lg:min-h-[80%]  justify-between items-center">
-        <div className="w-full items-end text-end p-4">
-          <Link to="/gallery">
-            <span className="font-bold text-3xl">X</span>
-          </Link>
+    <div className="flex-col font-raleway text-primary lg:bg-milk bg-gray lg:p-8 w-full lg:min-h-[80%]  justify-between items-center">
+      <div className="w-full items-end text-end p-4">
+        <Link to="/gallery">
+          <span className="font-bold text-3xl">X</span>
+        </Link>
+      </div>
+      <div className="lg:flex">
+        <div className="lg:w-[50%] my-6 flex justify-center">
+          <img 
+            className="max-w-[350px] lg:max-w-full lg:max-h-[500px] object-contain" 
+            src={meme.image} 
+            alt={meme.name} 
+          />
         </div>
-        <div className="lg:flex">
-          <div className="lg:w-[50%] my-6 lg:border-2 flex justify-center lg:border-gray">
-            <img 
-              className="max-w-[350px] lg:max-w-full lg:max-h-[500px] object-contain" 
-              src={meme.image} 
-              alt={meme.name} 
-            />
+        <div className="w-full lg:ml-6 max-lg:mt-6 max-lg:bg-milk max-lg:rounded-t-3xl max-lg:p-4">
+          <div>
+            <p>
+              <span className="font-bold">Fecha: </span>
+              {meme.date}
+            </p>
+            <p>
+              <span className="font-bold">Autor: </span>
+              {meme.author}
+            </p>
+            <p>
+              <span className="font-bold">Corriente: </span>
+              {meme.stream}
+            </p>
           </div>
-          <div className="w-full lg:ml-6 max-lg:mt-6 max-lg:bg-milk max-lg:rounded-t-3xl max-lg:p-4">
-            <div>
-              <p>
-                <span className="font-bold">Fecha: </span>
-                {meme.date}
-              </p>
-              <p>
-                <span className="font-bold">Autor: </span>
-                {meme.author}
-              </p>
-              <p>
-                <span className="font-bold">Corriente: </span>
-                {meme.stream}
-              </p>
-            </div>
-            <div className="flex flex-col">
-              <h1 className="text-3xl font-bold font-bodoni my-6">
-                {meme.name}
-              </h1>
-              <h2 className="text-2xl font-bodoni ">Descripcion</h2>
-              <p className="text-xl">{meme.description}</p>
-            </div>
-            <div className="w-full flex justify-center gap-6 mt-12 max-lg:pb-12">
-              <Link to={`/gallery/editmeme/${meme.id}`}>
-                <button className="rounded-3xl font-bodoni border py-2 px-6 flex ">
-                  Editar
-                </button>
-              </Link>
-              <button
-                onClick={MemeDelet}
-                className="rounded-3xl font-bodoni border py-2 px-6 flex  bg-primary text-milk"
-              >
-                Eliminar
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold font-bodoni my-6">
+              {meme.name}
+            </h1>
+            <h2 className="text-2xl font-bodoni ">Descripcion</h2>
+            <p className="text-xl">{meme.description}</p>
+          </div>
+          <div className="w-full flex justify-center gap-6 mt-12 max-lg:pb-12">
+            <Link to={`/gallery/editmeme/${meme.id}`}>
+              <button className="rounded-3xl font-bodoni border py-2 px-6 flex ">
+                Editar
               </button>
-            </div>
+            </Link>
+            <button
+              onClick={MemeDelet}
+              className="rounded-3xl font-bodoni border py-2 px-6 flex  bg-primary text-milk"
+            >
+              Eliminar
+            </button>
           </div>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default MemeDetail;
