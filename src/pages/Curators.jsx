114f68541bd993dd2f881239151bd233a6929fbc
DarthVada36 <velazquez.vada@outlook.com>
2024-09-18 12:26:12 +0200

    Curators

--- src/pages/Curators.jsx
+++ src/pages/Curators.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-
     return (
         <div className="w-full min-h-screen flex flex-col items-center bg-primary mt-[90px]">
             <div>
                 {/* Curador 1 */}
-                <div className="w-[300px] h-[250px] bg-transparent">
-
-                    <img src="../src/img/pngwingom.png" alt="marco" className="bg-transparent absolute w-[280px] h-[320px]" />
-                    <img src="../src/img/doge2.jpg" alt="" className="bg-transparent w-[280px] h-[320px]" />
-                    
+                <div className="relative w-[300px] h-[370px] bg-transparent mt-12">
+                    {/* Curator */}
+                    <img src="../src/img/doge2.jpg" alt="curator1" className="absolute top-0 overflow-hidden left-0 w-[280px] h-[350px] object-cover" />
+                    {/* Marco */}
+                    <img src="../src/img/pngwingom.png" alt="marco" className="absolute top-0  left-0 w-full h-full bg-transparent pointer-events-none" />
                 </div>
-                
-                
             </div>
         </div>
     );
 }
 
 export default Home;
+
